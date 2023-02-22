c32fdec6fa6f2c0c7a7bf2f88460268e2f91f5d6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-02-22 12:54:49 -0500

    [quality] Fix that Jpeg::Decoder had to stay alive

--- Bendage/JiPeg.cpp
+++ Bendage/JiPeg.cpp
@@ -1,21 +1,20 @@
 #include "JiPeg.hpp"
+
 #include "jpeg_decoder.h"
 #include "toojpeg.h"
+
 #include <iostream>
 namespace Bendage
 {
-JiPeg::JiPeg()
-{
-
-}
+JiPeg::JiPeg() { }
 
 void JiPeg::operator()()
 {
   auto& in_tex = inputs.tex.texture;
   auto& out_tex = outputs.tex.texture;
-  if (in_tex.bytes == nullptr)
+  if(in_tex.bytes == nullptr)
     return;
-  if (!in_tex.changed)
+  if(!in_tex.changed)
     return;
 
   rgb.clear();
@@ -25,7 +24,7 @@ void JiPeg::operator()()
   {
     // RGBA -> RGB for the input
     int bts = in_tex.width * in_tex.height * 4;
-    for(int rgb_i = 0, rgba_i = 0; rgba_i < bts; )
+    for(int rgb_i = 0, rgba_i = 0; rgba_i < bts;)
     {
       rgb_source[rgb_i++] = in_tex.bytes[rgba_i++];
       rgb_source[rgb_i++] = in_tex.bytes[rgba_i++];
@@ -38,40 +37,32 @@ void JiPeg::operator()()
   bytes.clear();
   bytes.reserve(in_tex.width * in_tex.height * 3);
 
-  auto wr = [] (void* self, unsigned char c) {
-    auto& s = *(JiPeg*) self;
+  auto wr = [](void* self, unsigned char c) {
+    auto& s = *(JiPeg*)self;
 
     s.bytes.push_back(c);
   };
 
   //for(int i = 0; i < 5; i++)
-  {
-    // To JPEG
-    TooJpeg::writeJpeg(this, wr,
-                       rgb_source,
-                       in_tex.width,
-                       in_tex.height,
-                       true,
-                       inputs.quality,
-                       true
-    );
 
+  // To JPEG
+  TooJpeg::writeJpeg(
+      this, wr, rgb_source, in_tex.width, in_tex.height, true, inputs.quality, true);
 
-    // From JPEG
-    Jpeg::Decoder decoder(bytes.data(), texture_bytesize);
-    if(decoder.GetResult() != Jpeg::Decoder::OK)
-      return;
+  // From JPEG
+  Jpeg::Decoder decoder(bytes.data(), texture_bytesize);
+  if(decoder.GetResult() != Jpeg::Decoder::OK)
+    return;
 
-    bytes.clear();
-    rgb_source = decoder.GetImage();
-  }
+  bytes.clear();
+  rgb_source = decoder.GetImage();
 
   outputs.tex.create(in_tex.width, in_tex.height);
 
   {
     // RGB -> RGBA for the output
     auto res = rgb_source;
-    for(int rgb_i = 0, rgba_i = 0; rgb_i < texture_bytesize; )
+    for(int rgb_i = 0, rgba_i = 0; rgb_i < texture_bytesize;)
     {
       out_tex.bytes[rgba_i++] = res[rgb_i++];
       out_tex.bytes[rgba_i++] = res[rgb_i++];
@@ -84,7 +75,5 @@ void JiPeg::operator()()
   out_tex.changed = true;
 }
 }
-
-
 
 #include "toojpeg.cpp"
